410851dd9b507ce6d2feea4485538f2f8281ac13
jabulani-creator <charingabiggie@gmail.com>
2023-07-24 22:55:19 -0700

    added main.h,_putchar.c,print_numbers, str_manipulation, parser.c

---
+++ print_numbers.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * print_numbers - Main Entry.
+ *
+ * Description: prints a number send to this function
+ *
+ * @args: List of arguments
+ *
+ * Return: The number of arguments printed
+ */
+
+int print_numbers(va_list args)
+{
+	int n;
+	int div;
+	int len;
+	unsigned int num;
+
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _putchar(45);
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _write_char(48 + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
+}
+
+/**
+ * print_unsgined_number - Entry Point.
+ *
+ * Description: Prints an unsigned number
+ *
+ * @n: unsigned integer to be printed
+ *
+ * Return: The amount of numbers printed
+ */
+
+int print_unsgined_number(unsigned int n)
+{
+	int div;
+	int len;
+	unsigned int num;
+
+	div = 1;
+	len = 0;
+
+	num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _write_char(48 + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
+}
